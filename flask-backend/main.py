3f97fb428d48cf89abc5adee88762f89a7c61a18
Akansha0211 <akansha.saxena0211@gmail.com>
2021-05-16 19:45:03 +0530

    Making post req to endpoint /

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template("index.html",token="Hello Flask_React")
+
+@app.route('/', methods=['POST'])
+def register():
+    return "Hello Akansha"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- react-app/src/Register.js
+++ react-app/src/Register.js
@@ -3,16 +3,18 @@ import {Form} from 'react-bootstrap';
 import axios from 'axios';
 
 function Register() {
-    const [usrename, setUsername] = useState('')
+    const [username, setUsername] = useState('')
     const register = () =>{
-        
+        axios.post("http://localhost:5000/",{
+            username : username
+        })
     }
     return (      
         <div>
-           <Form method="POST" action="">
+           <Form method="POST" action="/">
                <div className="form-group">
                    <label for="username">Username</label>
-                   <input type="text" name="username" autoComplete="off" required></input>
+                   <input type="text" id="username" name="username" autoComplete="off" required value={username} onChange={e=>setUsername(e.target.value)}></input>
                </div>
                <div className="form-group">
                    <button type="submit" className="btn btn-primary" onClick={register}>Register Me</button>
